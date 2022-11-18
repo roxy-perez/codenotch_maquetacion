abf21a114969631a5ddf1ed26221aa877d255a48
RoxyWTF <necropsia32@gmail.com>
2022-11-18 18:42:12 +0000

    adding travel agency

--- travel_agency/js/app.js
+++ travel_agency/js/app.js
@@ -1,24 +1,52 @@
 const submit = document.getElementById("submit");
+const filter = document.getElementById("search");
 
-const getFormData = () => {
-    const form = document.querySelector('#travel-form');
-    return new FormData(form);
+const name = document.getElementById("name");
+const origin = document.getElementById("origin");
+const destination = document.getElementById("destination");
+const nPeople = document.getElementById("nPeople");
+const date = document.getElementById("date");
+
+function clearInputs() {
+    name.value = "";
+    origin.value = "";
+    destination.value = "";
+    nPeople.value = "";
+    date.value = "";
 }
 
-const toJson = function (event) {
-    const formData = getFormData();
-    event.preventDefault();
-    let object = {};
-    let arrForm = [];
-    formData.forEach((value, key) => {
-        if (value != undefined) {
-            object[key] = value;
-        }
-    });
+const saveDataForm =  (event) => {
+    event.preventDefault(); //Evitamos el comportamiento por defecto
 
-    let json = JSON.stringify(object);
-    arrForm.push(json);
-    console.log(arrForm);
-};
+    let _name = name.value;
+    let _origin = origin.value;
+    let _destination = destination.value;
+    let _nPeople = nPeople.value;
+    let _date = date.value;
 
-submit.addEventListener("click", toJson);
+    let data = {
+        name: _name,
+        origin: _origin,
+        destination: _destination,
+        nPeople: _nPeople,
+        date: _date
+    }
+
+    let arrDataForm = [];
+    arrDataForm.push(data);
+
+    clearInputs();
+
+
+    console.log(arrDataForm)
+
+}
+
+const showDataFilter = (event) => {
+    
+}
+
+submit.addEventListener("click", saveDataForm);
+
+search.addEventListener("click", showDataFilter);
+

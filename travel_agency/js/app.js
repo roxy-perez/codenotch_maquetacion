4fc10acaf61ac5f8f94aa68792a0d5901e2dbf7b
Roxy <necropsia32@gmail.com>
2022-11-19 02:59:21 +0000

    Agencia terminada - Rama Day3

--- travel_agency/js/app.js
+++ travel_agency/js/app.js
@@ -7,6 +7,8 @@ const destination = document.getElementById("destination");
 const nPeople = document.getElementById("nPeople");
 const date = document.getElementById("date");
 
+let arrDataForm = [];
+
 function clearInputs() {
     name.value = "";
     origin.value = "";
@@ -15,12 +17,12 @@ function clearInputs() {
     date.value = "";
 }
 
-const saveDataForm =  (event) => {
+const saveDataForm = (event) => {
     event.preventDefault(); //Evitamos el comportamiento por defecto
 
     let _name = name.value;
     let _origin = origin.value;
-    let _destination = destination.value;
+    let _destination = destination.value.toString();
     let _nPeople = nPeople.value;
     let _date = date.value;
 
@@ -32,21 +34,64 @@ const saveDataForm =  (event) => {
         date: _date
     }
 
-    let arrDataForm = [];
     arrDataForm.push(data);
-
     clearInputs();
-
-
     console.log(arrDataForm)
 
 }
 
+const isCommunity = city => city.destination.toLowerCase() === 'Canarias'.toLowerCase()
+    || city.destination.toLowerCase() === 'Mallorca'.toLocaleLowerCase()
+    || city.destination.toLowerCase() === 'Galicia'.toLowerCase();
+
+
 const showDataFilter = (event) => {
-    
+    event.preventDefault();
+
+    let result = arrDataForm.filter(isCommunity);
+    let list = document.getElementById("list");
+    list.innerHTML = `<tr>
+                        <th>Name</th>
+                        <th>Origin</th>
+                        <th>Destination</th>
+                        <th>Number of People</th>
+                        <th>Departure date</th>
+                     </tr>
+                     `
+    for (var i = 0; i < result.length; i++) {
+        list.innerHTML += `
+        <tr>
+            <td>${result[i].name.toUpperCase()}</td>
+            <td>${result[i].origin.toUpperCase()}</td>
+            <td>${result[i].destination.toUpperCase()}</td>
+            <td>${result[i].nPeople.toUpperCase()}</td>
+            <td>${result[i].date.toUpperCase()}</td>
+         </tr>`
+    };
+}
+
+function clean() {
+    let list = document.getElementById("list");
+    list.innerHTML = `<tr>
+                            <th>Name</th>
+                            <th>Origin</th>
+                            <th>Destination</th>
+                            <th>Number of People</th>
+                            <th>Departure date</th>
+                        </tr>
+ 
+        <tr>
+            <td>Name</td>
+            <td>Origin</td>
+            <td>Destination</td>
+            <td>Number of People</td>
+            <td>Departure date</td>
+         </tr>`;
+
+
 }
 
 submit.addEventListener("click", saveDataForm);
 
-search.addEventListener("click", showDataFilter);
+filter.addEventListener("click", showDataFilter);
 
